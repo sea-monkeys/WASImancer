6de2ecfdff90ee295df8c7a5c5982f089f71d744
@k33g <ph.charriere@gmail.com>
2025-04-18 07:35:05 +0200

    🛟 Updated.

--- env-var-utils.js
+++ env-var-utils.js
@@ -1,19 +1,8 @@
 
-
 /**
  * 
- * @returns {Array} List of environment variables with a name starting with "WASM_" and a value that is not empty
+ * @returns {Object} Object with environment variables with a name starting with "WASM_" and a value that is not empty
  */
-export function _getWasmEnvVarsList() {
-    // returns the list of the environment variables with a name starting with "WASM_"
-    // and a value that is not empty
-    const envVars = Object.entries(process.env)
-        .filter(([key, value]) => key.startsWith("WASM_") && value)
-        .map(([key, value]) => ({ name: key, value: value }));
-    return envVars;
-}
-
-// same function but return an object with the env var name as key and the value as value
 export function getWasmEnvVarsList() {
     // returns the list of the environment variables with a name starting with "WASM_"
     // and a value that is not empty
